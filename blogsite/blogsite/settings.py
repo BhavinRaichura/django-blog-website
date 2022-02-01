2f841a4e9fbf0988453ea9d145df9b318b49bfd5
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-02-02 02:30:42 +0530

    Update settings.py

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #<<<<<<< HEAD
-SECRET_KEY = 'django-insecure-e$c*u-u#99mjkhavouf%)=!sy_q4yztf(#9)bsdmt_8jsfcle-'
+#SECRET_KEY = 'django-insecure-e$c*u-u#99mjkhavouf%)=!sy_q4yztf(#9)bsdmt_8jsfcle-'
 #SECRET_KEY =''
 #SECRET_KEY = ''
 
